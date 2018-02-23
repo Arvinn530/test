50080401d01bb3139ce9183b06d540356b9c8686
Arvinn530 <32431069+Arvinn530@users.noreply.github.com>
2018-02-23 13:11:33 +0700

    Update LearningProject.java

--- LearningProject.java
+++ LearningProject.java
@@ -1,55 +1,118 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class LearningProject {
-	public static boolean isPalindrom (String str)  {
-        if (str.length() <= 1) {
-            return true;
-        } else if (str.charAt(0)==str.charAt(str.length()-1)) {
-            return isPalindrom(str.substring(1, str.length()-1));
-        } else {
-            return false;
+	class Manusia {
+        private String nama;
+        private byte umur;
+        public long uang = 50000;
+        public float kebahagiaan = 50.0;
+        Manusia(String nama, byte umur)) {
+            this.nama = nama;
+            this.umur = umur;
+        }
+
+        Manusia(String nama, byte umur, long uang) {
+            this.nama = nama;
+            this.umur = umur;
+            this.uang = uang;
+        }
+
+        private float ubahKebahagiaan(float kebahagiaan) {
+            if (kebahagiaan > 100.0) {
+                return 100.0;
+            } else if (kebahagiaan < 0) {
+                return 0;
+            } else {
+                return kebahagiaan;
+            }
+        }
+
+        public void beriUang(Manusia penerima) {
+            int jumlah = 0;
+            for(int i=0; i<nama.length(); i++) {
+                jumlah += (int) nama.charAt(i);
+            }
+            jumlah *= 100;
+            if (uang < jumlah) {
+                System.out.println(this.nama + " ingin memberi uang kepada " +
+                    penerima.nama + " namun tidak memiliki cukup uang :'(");
+            } else {
+                this.uang -= jumlah;
+                penerima.uang += jumlah;
+                this.kebahagiaan = this.ubahKebahagiaan(jumlah / 6000)
+                penerima.kebahagiaan = this.ubahKebahagiaan(jumlah / 6000)
+                System.out.println(this.nama + " memberi uang sebanyak " + 
+                    jumlah + " kepada " + penerima.nama + ", lalu mereka berdua senang"); 
+            }
+            return ;
+        }
+
+        public void beriUang(Manusia penerima, int jumlah) {
+            if (uang < jumlah) {
+                System.out.println(this.nama + " memberi uang ");
+            } else {
+                this.uang -= jumlah;
+                penerima.uang += jumlah;
+                this.kebahagiaan = this.ubahKebahagiaan(this.kebahagiaan + jumlah / 6000);
+                penerima.kebahagiaan = this.ubahKebahagiaan(penerima.kebahagiaan + jumlah / 6000);
+                System.out.println(this.nama + " memberi uang sebanyak " + 
+                    jumlah + " kepada " + penerima.nama + ", lalu mereka berdua senang"); 
+            }
+            return ;
+        }
+
+        public void bekerja(int durasi, float bebanKerja) {
+            if (this.umur < 19) {
+                System.out.println(this.nama + " belum boleh bekerja karena masih dibawah umur D:");
+                return ; 
+            }
+            float bebanKerjaTotal;
+            int pendapatan;
+            if (bebanKerjaTotal > this.kebahagiaan) {
+                durasi = (int) kebahagiaan / bebanKerja;
+                bebanKerjaTotal = durasi * bebanKerja;
+                pendapatan = bebanKerjaTotal * 10000;
+                this.uang +=  pendapatan;
+                System.out.println(this.nama + 
+                    " tidak bekerja secara full time karena sudah terlalu lelah, total pendapatan :  " + 
+                    pendapatan);  
+            } else {
+                bebanKerjaTotal = durasi * bebanKerja;
+                pendapatan = bebanKerjaTotal * 10000;
+                this.uang += pendapatan;
+                this.kebahagiaan -= bebanKerjaTotal;
+                System.out.println(this.nama + " bekerja full time, total pendapatan : " + pendapatan);
+            } 
+            return ;
+        }
+
+        public void rekreasi(String namaTempat) {
+            int biaya = namaTempat.length() * 10000;
+            if (this.uang < biaya) {
+                System.out.println(this.nama + " tidak mempunyai cukup uang untuk berekreasi di " + 
+                    namaTempat + " :(");
+            } else {
+                this.uang -= biaya;
+                this.kebahagiaan = ubahKebahagiaan(this.kebahagiaan - namaTempat.length());
+                System.out.println(this.nama + " berekreasi di " + namaTempat + ", " + 
+                    this.nama + " senang :)");
+            }
+        }
+
+        public void sakit(String penyakit) {
+            this.kebahagiaan = ubahKebahagiaan(this.kebahagiaan - penyakit.length());
+            System.out.println(this.nama + "terkena penyakit " + penyakit);
+        }
+
+        public void toString() {
+            System.out.println("Nama\t\t: " + this.nama + "\n" + 
+                "Umur\t\t: " + this.umur + "\n" +
+                "Uang\t\t: " + this.uang + "\n" +
+                "Kebahagiaan\t\t: " + this.kebahagiaan);
         }
     }
-
-    public static int normal(int num) {
-        if (num == 1) {
-        	return 1;
-       	} 
-        else {
-        	return 1 + num * normal(num - 1);
-        } 
-    }
-
-    public static int combination(String str, int ix) {
-        if (ix == str.length()) {
-            return 0;
-        } else {
-            String str1 = str.substring(0, ix) + str.substring(ix+1);
-            return palindrom(str1) + combination(str, ix + 1);
-        }
-    }
-
-    public static int palindrom (String str) {
-        if (str.length() == 1 || isPalindrom(str)) {
-            return 1;
-        } else {
-            return 1 + combination(str, 0);
-        }
-    }
-
 	public static void main(String args[]) {
-		Scanner input = new Scanner(System.in);
-		String str0, str1;
-		str0 = "continue";
-		while(!str0.equals("stop")) {
-			str0 = input.next();
-			str1 = input.next();
-			if(str0.equals("normal")) {
-				System.out.println(normal(str1.length()));
-			} else if(str0.equals("palindrom")) {
-				System.out.println(palindrom(str1));
-			}	
-		}
+
 	}
+
 }
